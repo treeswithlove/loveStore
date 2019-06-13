eb655059764973b3fcc3311a01eaf74504833493
treeswithlove <monet.2017@gmail.com>
2019-06-13 19:17:35 -0400

    registered routes

---
+++ loveStore_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from . import views
+
+router = routers.DefaultRouter()
+router.register('orders', views.OrderView)
+router.register('users', views.UserView)
+router.register('shells', views.ShellView)
+router.register('users', views.UserView)
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
