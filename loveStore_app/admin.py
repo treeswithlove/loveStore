f88a7e5593d500f9db94616f864bfe0f4c217317
treeswithlove <monet.2017@gmail.com>
2019-06-13 10:36:08 -0400

    added models to the admin

--- loveStore_app/admin.py
+++ loveStore_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Item, Shell, Order
+
+admin.site.register(User)
+admin.site.register(Item)
+admin.site.register(Shell)
+admin.site.register(Order)
