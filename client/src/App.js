dd9663323d27b39cc172bc168859225b90820d32
treeswithlove <monet.2017@gmail.com>
2019-06-13 10:41:14 -0400

    added some switch roots and started nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import ShellList from "./components/shellList";
+import Shell from "./components/shell";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+
+                    <div>
+                        <h1>Love Shop</h1>
+                        <nav>
+                            <div><Link to="/shell">Shells Necklaces</Link></div>
+                            <div><Link to="/user">User Login</Link></div>
+                        </nav>
+                    </div>
+ 
+                     <Switch>
+                       <Route exact path="/shell" component={ShellList}/>
+                       <Route path="/shell/:id" component={Shell}/>
+                     </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
