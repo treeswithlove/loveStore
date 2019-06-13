6aa7521dcff9ec88badfef0cb2e21fb65a881eca
treeswithlove <monet.2017@gmail.com>
2019-06-13 10:37:26 -0400

    added views

--- loveStore_app/views.py
+++ loveStore_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import ShellSerializer, OrderSerializer, ItemSerializer, UserSerializer
+from .models import User, Shell, Item, Order
+
+
+class ShellView(viewsets.ModelViewSet):
+    queryset = Shell.objects.all()
+    serializer_class = ShellSerializer
+
+class OrderView(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class ItemView(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class =ItemSerializer
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
