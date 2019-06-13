30344d60c08d10bc85e3689ffb7b39074a85e1ba
treeswithlove <monet.2017@gmail.com>
2019-06-13 10:29:39 -0400

    made changes to models

--- loveStore_app/models.py
+++ loveStore_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(default = '', max_length = 30)
+    last_name = models.CharField(default = '', max_length = 30)
+    email = models.EmailField(default = '',  max_length = 50)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+class Shell(models.Model):
+    name = models.CharField(default = '', max_length = 100)
+    image_url = models.CharField(default = '', max_length = 512)
+    purchase_status = (
+        ('Reserved', 'Reserved'),
+        ('Available', 'Available'),
+        ('Out of Stock', 'Unavailable'), 
+    )
+    status_field = models.CharField( max_length=25, choices=purchase_status, blank=True, default='', help_text='Shell availability',)
+    description = models.TextField(default = '', max_length = 1000)
+    price = models.DecimalField(default = '00.00', max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    name = models.CharField(default = '', max_length = 30)
+
+    def __str__(self):
+        return self.name
+    
+class Item(models.Model):
+    shell = models.ForeignKey(Shell, on_delete = 'CASCADE', related_name = 'Item')
+    user = models.ForeignKey(User, on_delete = 'CASCADE', related_name = 'Item')
+    quantity = models.IntegerField(default = '')
+    order = models.ForeignKey(Order, on_delete = 'CASCADE', related_name = 'Item')
