fe2541efd7377a1168b62d71880343f98a558bd4
treeswithlove <monet.2017@gmail.com>
2019-06-14 23:03:14 -0400

    api working

--- loveStore_app/views.py
+++ loveStore_app/views.py
@@ -4,6 +4,13 @@ from .serializers import ShellSerializer, OrderSerializer, ItemSerializer, UserS
 from .models import User, Shell, Item, Order
 from django.views import View
 from django.http import JsonResponse
+import stripe
+from decouple import config
+
+stripe.api_key = config("API_KEY")
+
+
+
 
 
 class ShellView(viewsets.ModelViewSet):
@@ -26,5 +33,36 @@ class StripeView(View):
     def get(self, request, *args, **kwargs):
         shell = request.GET['shell']
         quantity = request.GET['quantity']
-        json = {"test":"the", 'shell': shell, 'quantity': quantity}
-        return JsonResponse(json, safe=False)
+        result = stripe.Charge.list()
+        createdOrder = stripe.Order.create(
+            currency = 'usd',
+            # items= [
+            #     {
+            #         "description": "test"
+            #         # "amount": 3,
+            #         # "type": "sku",
+            #         # "parent":'sku_FFwXDIb7oLRG1B'
+
+            #     }
+            # ]
+            items = [
+                {
+                    "type": "sku",
+                    "parent": "sku_FFy2POOAuAq2iT"
+
+                }
+            ],
+             shipping={
+    "name":'Jenny Rosen',
+    "address":{
+      "line1":'1234 Main Street',
+      "city":'San Francisco',
+      "state":'CA',
+      "country":'US',
+      "postal_code":'94111'
+    },
+  },
+        )
+        print(result)
+        # json = {"test":"the", 'shell': shell, 'quantity': quantity}
+        return JsonResponse(result, safe=False)
